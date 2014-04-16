71abf96929ea3c44d852afbdbe34b51bc8adf8fb
KangJin <ranian129@gmail.com>
2014-04-16 20:04:10 +0900

    update subject servlets.
    1) 페이지에 링크 추가
    2) Redirect 심기
    3) CharacterEncodingFilter 적용

---
+++ semsx/src/servlets/subject/SubjectDeleteServlet.java
@@ -0,0 +1,72 @@
+package servlets.subject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import dao.SubjectDao;
+
+/* Refresh
+ * 	- 일정 시간 후 서버에 지정된 URL을 요청하게 만듬.
+ * 	1) 응답 헤더에 리프래시 정보 심기
+ * 	2) HTML 헤더에 리플래시 정보 심기
+ * 
+ * Redirect
+ * 	- 클라이언트에게 다시 요청할 주소를 알려줌.
+ * 	- 경과 시간 지정불가.
+ * 	- 콘텐츠를 보내지 않는다는 장점!
+ */
+
+@WebServlet("/subject/delete.bit")
+@SuppressWarnings("serial")
+public class SubjectDeleteServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+		
+		// CharsetEncodingFilter로 대체함
+		//request.setCharacterEncoding("UTF-8");
+		
+		// 1) 응답 헤더에 리프래시 명령어 심기 
+		//response.setHeader("Refresh", "1;url=list.bit?pageNo=1&pageSize=10");
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>과목삭제</title>");
+		
+		// 2) 응답 내용에 Refresh 명령어 심기
+		//out.println("meta http-equiv='Refresh'"
+		//		+ " content='1;url=list.bit?pageNo=1&pageSize-10'>"); // 자동 지뢰
+		out.println("</head><body>");
+		
+		try {
+			out.println("<h1>과목 삭제 결과</h1>");
+			
+			SubjectDao dao = (SubjectDao)this.getServletContext().getAttribute("subjectDao");
+			
+			int no = Integer.parseInt(request.getParameter("no"));
+			
+			dao.delete(no);
+
+			out.println("삭제 성공");
+			
+			// 3. Redirect 처리
+			//	- 콘텐츠를 출력하지 않기 때문에 => 이전에 출력은 취소된다.
+			response.sendRedirect("list.bit?pageNo=1&pageSize=10");
+
+		} catch (Throwable e) {
+			out.println("오류 발생 했음.");
+			e.printStackTrace();
+		}
+		
+		out.println("</body></html>");
+		
+	}
+	
+}
---
+++ semsx/src/servlets/subject/SubjectDetailServlet.java
@@ -0,0 +1,75 @@
+package servlets.subject;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import vo.SubjectVo;
+import dao.SubjectDao;
+
+// 목록으로 가기, 삭제하기 링크 추가
+
+@WebServlet("/subject/detail.bit")
+@SuppressWarnings("serial")
+public class SubjectDetailServlet extends HttpServlet {
+	
+	@Override
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+	    throws ServletException, IOException {
+		
+		response.setContentType("text/html;charset=UTF-8");
+		PrintWriter out = response.getWriter();
+		out.println("<html><head><title>과목상세정보</title></head><body>");
+		
+		try {
+			out.println("<h1>과목 상세정보</h1>");
+			
+			SubjectDao dao = (SubjectDao)this.getServletContext().getAttribute("subjectDao");
+			
+			int no = Integer.parseInt(request.getParameter("no"));
+
+			SubjectVo subject = dao.detail(no);
+
+			out.println("<table border='1'>");
+			
+			out.println("<tr>");
+			out.println("	<th>번호</th>");
+			out.println("	<td>" + subject.getNo() + "</td>");
+			out.println("</tr>");
+
+			out.println("<tr>");
+			out.println("	<th>과목명</th>");
+			out.println("	<td>" + subject.getTitle() + "</td>");
+			out.println("</tr>");
+			
+			out.println("<tr>");
+			out.println("	<th>내용</th>");
+			out.println(" <td><textarea rows='5' cols='60'>"
+					+ subject.getDescription()
+					+ "</textarea></td>");
+			out.println("</tr>");
+			
+			out.println("</table>");
+			out.println("<a href='list.bit?pageNo=1&pageSize=10'>목록</a> ");
+			out.println("<a href='delete.bit?no="
+					+ subject.getNo()
+					+ "'>삭제</a> ");
+			out.println("<a href='update.bit?no="
+					+ subject.getNo()
+					+ "'>변경</a><br>");
+
+		} catch (Throwable e) {
+			out.println("오류 발생 했음.");
+			e.printStackTrace();
+		}
+		
+		out.println("</body></html>");
+		
+	}
+	
+}
